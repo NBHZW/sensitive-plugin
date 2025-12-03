04ef269ef6a911f8c6ee3e93eeab80dcec609db8
ZWH266183351 <12242836+zwh266183351@user.noreply.gitee.com>
2025-12-03 17:08:11 +0800

    feat(enums): 添加多种替换方法枚举成员
    - 新增 Name 替换方法枚举项，绑定 NameReplacement 类
    - 新增 CardIdStrict 替换方法枚举项，绑定 CardIdStrictReplacement 类
    - 新增 CardId 替换方法枚举项，绑定 CardIdReplacement 类
    - 新增 Email 替换方法枚举项，绑定 EmailReplacement 类
    - 修改枚举类以支持新的替换方法类型

--- src/main/kotlin/enums/ReplacementMethodEnum.kt
+++ src/main/kotlin/enums/ReplacementMethodEnum.kt
@@ -1,10 +1,17 @@
 package enums
 
 import ReplacementMethod
+import replacement_methods.CardIdReplacement
+import replacement_methods.CardIdStrictReplacement
+import replacement_methods.EmailReplacement
+import replacement_methods.NameReplacement
 import kotlin.reflect.KClass
 
 
 enum class ReplacementMethodEnum(val clazz: KClass<out ReplacementMethod>) {
     Default(AllReplacement::class),
-
+    Name(NameReplacement::class),
+    CardIdStrict(CardIdStrictReplacement::class),
+    CardId(CardIdReplacement::class),
+    Email(EmailReplacement::class),
 }
