b1c582503bf32d8f133336b4c1f4758a27cb033a
ZWH266183351 <12242836+zwh266183351@user.noreply.gitee.com>
2025-12-03 15:16:39 +0800

    fix(replacement): 修复敏感方法返回类型
    - 将 sensitive 方法的返回类型由 String 修改为可空类型 String?
    - 提升方法返回值的灵活性以支持空值情况
    - 保持方法参数和注释不变，聚焦类型修正

--- src/main/kotlin/ReplacementMethod.kt
+++ src/main/kotlin/ReplacementMethod.kt
@@ -2,7 +2,7 @@ interface ReplacementMethod {
     /**
      * 脱敏方法
      */
-    fun sensitive(fieldValue: Any?, replaceChar: Char, rule: Array<String>): String
+    fun sensitive(fieldValue: Any?, replaceChar: Char, rule: Array<String>): String?
 
     /**
      * 是否清空字符串
